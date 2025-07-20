2bd9f93387b6cb9b8d0c9dab94d2485799e9bf8e
Luke „Midnight“ <Mr.hahaah@gmail.com>
2025-07-20 20:22:56 +0000

    Still nothing, any other ideas?

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  // add more options here
+  register: true,
+  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
